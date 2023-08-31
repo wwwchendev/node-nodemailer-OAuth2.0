71d895b3453c7f9778a0789b6392db9b597a7752
wwwchendev <wwwchendev@gmail.com>
2023-08-31 15:12:28 +0800

    wip: 新增email路由與視圖 index添加以google帳號登入按鈕

--- routes/email.js
+++ routes/email.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home Page' });
+router.get('/user', function(req, res, next) {
+  res.render('emailUser', { title: 'Email-User Page' });
 });
 
 module.exports = router;
